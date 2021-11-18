e483c79e99c261393b7e59f83636f3a0f75564e8
kwamboka1 <makorimildred@gmail.com>
2021-11-18 17:00:14 +0300

    Update 3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for ch in range(97, 123):
+for alphabet in range(97, 123):
         if alphabet != 101 and alphabet != 113:
                 print("{:c}".format(alphabet), end='')
