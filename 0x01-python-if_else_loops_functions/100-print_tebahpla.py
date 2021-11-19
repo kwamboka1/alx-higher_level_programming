c73283f98e93c9564906ca52735a9c5fe54f8a56
kwamboka1 <makorimildred@gmail.com>
2021-11-19 13:00:57 +0300

    Update 100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for alphabet in reversed(range(97, 123)):
-    print("{:c}".format(alphabet if (alphabet % 2 == 0) else (alphabet - 32)), end='')
+for ch in reversed(range(97, 123)):
+    print("{:c}".format(ch if (ch % 2 == 0) else (ch - 32)), end='')
