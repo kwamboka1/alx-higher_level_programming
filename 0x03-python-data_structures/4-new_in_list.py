98887f110d8c0c70ace72224598c3f9bbfa517c8
kwamboka1 <makorimildred@gmail.com>
2021-11-22 07:21:07 +0000

    Modify 4-new_in_list.py file

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-def print_reversed_list_integer(my_list=[]):
+def new_in_list(my_list, idx, element):
+    length = len(my_list)
 
-    if not my_list:
-        pass
-    else:
-        my_list.reverse()
-        for i in range(len(my_list)):
-            print("{:d}".format(my_list[i]))
+    new_list = my_list[:]
+
+    if 0 <= idx < length:
+        new_list[idx] = element
+
+        return (new_list)
