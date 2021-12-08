357b6ecef157c3173f140543d3c03de94f992e24
kwamboka1 <makorimildred@gmail.com>
2021-12-08 11:23:57 +0000

    Modify 4-square file

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -3,8 +3,8 @@
 
 
 class Square:
+    """ Method to initialize the square object"""
     def __init__(self, size=0):
-        """ Method to initialize the square object"""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
