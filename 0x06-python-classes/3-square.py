1db9713c2da2310321b22be7cf754fee36f2767e
kwamboka1 <makorimildred@gmail.com>
2021-12-08 09:37:39 +0000

    Add docstrings

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -3,9 +3,8 @@
 
 
 class Square:
+    """ Method to initialize the square object"""
     def __init__(self, size=0):
-        """ Method to initialize the square object
-        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
@@ -14,6 +13,5 @@ class Square:
             self.__size = size
 
     def area(self):
-        """ Method that returns the square are of the object
-        """
+        """ Method that returns the square area of the object"""
         return (self.__size ** 2)
