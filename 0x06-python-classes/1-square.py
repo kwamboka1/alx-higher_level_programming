7e63e47dc1c780d3512f5fdac25d9f11b48bdeca
kwamboka1 <makorimildred@gmail.com>
2021-12-08 09:07:48 +0000

    Adding Docstrings

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-""""""Class Square that defines a square object""""""
+""""""Class Square that defines a square object
+""""""
 
 
 class Square:
-    def __init__(self, size):
     """Initialize method that stores the size of the square
 
         Args:
             param1 (int): size of the square
     """
+    def __init__(self, size):
         self.__size = size
