9f21d29185e6448d0b84af0cc784ecad267dc09c
kwamboka1 <makorimildred@gmail.com>
2021-12-08 09:30:07 +0000

    Add doctstrings

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
+""" A class that defines a square by its size"""
+
+
 class Square:
-    """ A class that defines a square by its size
-    """
+    """ Method to initialize the square object"""
     def __init__(self, size=0):
-        """ Method to initialize the square object
-        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
