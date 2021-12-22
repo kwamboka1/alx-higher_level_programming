e1f16660cf2ae4a63a5dd147693e0eff3d6f3fb3
kwamboka1 <makorimildred@gmail.com>
2021-12-22 08:44:47 +0000

    Implementing pycodestyle

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -4,10 +4,8 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    
     """ Initializes instance """
     def __init__(self, width, height):
-    
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
@@ -17,9 +15,6 @@ class Rectangle(BaseGeometry):
     def area(self):
 
         return self.__width * self.__height
-
-    """ Special/magical method that returns the printable string 
-    """
+    """ Special/magical method that returns the printable string"""
     def __str__(self):
-
-                return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
+        return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
