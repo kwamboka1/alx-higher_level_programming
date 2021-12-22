e00b107a3aeb855e8daf2d5da5bd398670edfc21
kwamboka1 <makorimildred@gmail.com>
2021-12-22 08:56:19 +0000

    Implementing pycoestyle

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,10 +6,8 @@
 
 
 class MyList(list):
-
     """ Method that prints the sorted list """
     def print_sorted(self):
-    
         l_sorted = self.copy()
         l_sorted.sort()
         print(l_sorted)
