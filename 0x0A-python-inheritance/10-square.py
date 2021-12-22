fa0fec1a0a726127fdf50dea06bc0a1ed1711cea
kwamboka1 <makorimildred@gmail.com>
2021-12-22 08:46:55 +0000

    Add docstring

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-Rectangle = __import__('9-rectangle').Rectangle
 """ Class that defines a Square from Rectangle class """
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
