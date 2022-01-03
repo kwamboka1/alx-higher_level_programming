4558b007bea300a076d674c4259e0a70ed4628b4
kwamboka1 <makorimildred@gmail.com>
2022-01-03 15:50:17 +0300

    Update 12-pascal_triangle.py

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -5,7 +5,7 @@
         n: number of lines
 
     Returns:
-        matrix: a matrix with the pascal triangle
+        Empty list: if n <= 0 Otherwise pascal triangle
 
 """
 
@@ -15,14 +15,14 @@ def pascal_triangle(n):
     if n <= 0:
         return ([])
 
-    pascal = [[1]]
+    pascal_t = [[1]]
     for i in range(1, n):
         row = [1]
-        prev = pascal[i - 1]
+        prev = pascal_t[i - 1]
         for j in range(len(prev)):
                 new = prev[j] + prev[j + 1] if j != len(prev) - 1 else 1
                 row.append(new)
 
-        pascal.append(row)
+        pascal_t.append(row)
 
-    return pascal
+    return pascal_t
