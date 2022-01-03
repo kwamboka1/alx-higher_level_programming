dfc8aa595a022ab666ba26a96f67f40c340602ff
kwamboka1 <makorimildred@gmail.com>
2022-01-03 13:49:42 +0300

    Update 12-pascal_triangle.py

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -2,26 +2,20 @@
 
 
 def pascal_triangle(n):
-    """ Function that returns the pascal triangle
-
-    Args:
-        n: number of lines
-
-    Returns:
-         Empty list: if n <= 0 otherwise pascal triangle
-
+    """ Function that creates a pascal triangle and
+    returns empty list if n <= 0
     """
     if n <= 0:
         return ([])
 
-    pascal_t = [[1]]
+    pascal = [[1]]
     for i in range(1, n):
         row = [1]
-        prev = pascal_t[i - 1]
+        prev = pascal[i - 1]
         for j in range(len(prev)):
                 new = prev[j] + prev[j + 1] if j != len(prev) - 1 else 1
                 row.append(new)
 
-        pascal_t.append(row)
+        pascal.append(row)
 
-    return pascal_t
+    return pascal
