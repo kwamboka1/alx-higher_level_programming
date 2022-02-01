e8c534b04fb00e7e6fd800084a9b163fbb31e5ad
kwamboka1 <makorimildred@gmail.com>
2022-02-01 14:45:07 +0000

    Add 4-first_table.sql file

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+-- Creates a table in the current database
+-- Query to create a table called first_table in the current database in my MySQL server
+
+CREATE TABLE IF NOT EXISTS first_table (
+id INT,
+name VARCHAR(256));
