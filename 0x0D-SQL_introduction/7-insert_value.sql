fef80c0869164f06819ee635112ad5e7787f137a
kwamboka1 <makorimildred@gmail.com>
2022-02-02 09:35:10 +0000

    [Feature]: A script that inserts a new row in a table

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,4 +1,4 @@
 -- Inserts a new row in a table
 -- Query to insert a new row in the table first_table
 
-INSERT INTO first_table (id, name) VALUES (89, "Holberton School");
+INSERT INTO first_table (id, name) VALUES (89, "Best School");
