e9a3e9d9cf6d3305e114615890743c51e449715a
kwamboka1 <makorimildred@gmail.com>
2022-02-28 11:07:54 +0000

    [Add]: A script that prints 3 lines

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const firstLanguage = 'C is fun';
+const secondLanguage = 'Python is cool';
+const thirdLanguage = 'JavaScript is amazing';
+
+console.log(firstLanguage);
+console.log(secondLanguage);
+console.log(thirdLanguage);
