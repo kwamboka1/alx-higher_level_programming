c465935bda27983835477e3baee16ac7198b79bb
kwamboka1 <makorimildred@gmail.com>
2022-03-01 08:48:27 +0000

    [Fix]: Code

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,13 @@
 #!/usr/bin/node
 
-let i = parseInt(process.argv[2]);
-while (i > 0) {
-  console.log('C is fun');
-  i--;
-} if (Number.isNaN(i)) {
+const count = parseInt(process.argv[2]);
+
+// Check if argument is NaN
+
+if (isNaN(count)) {
   console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < count; i++) {
+    console.log('C is fun');
+  }
 }
