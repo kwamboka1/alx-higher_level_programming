e0f7d775a3563d9248509d70195326ff840d2586
kwamboka1 <makorimildred@gmail.com>
2022-03-02 14:05:34 +0000

    [Fix]: Code

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -3,11 +3,10 @@
 const dict = require('./101-data').dict;
 const newDict = {};
 
-Object.keys(dict).map(function (key, value) {
-  if (newDict[dict[key]] === undefined) {
+for (const key in dict) {
+  if (typeof (newDict[dict[key]]) === 'undefined') {
     newDict[dict[key]] = [];
   }
   newDict[dict[key]].push(key);
-});
-
+}
 console.log(newDict);
